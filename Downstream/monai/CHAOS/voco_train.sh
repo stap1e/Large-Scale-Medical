a15b85932e8866121ae3d94787eb6fddc7dcd1c3
stap1e-laptap <1602564548@qq.com>
2026-08-07 23:53:04 +0800

    update some codes for downstreaming task v1.8

---
+++ Downstream/monai/CHAOS/voco_train.sh
@@ -0,0 +1,22 @@
+now=$(date +"%Y%m%d_%H%M%S")
+name=VoCo
+pretrained_root=/ai/workspace/pretrained/voco
+logdir=runs/chaos_voco
+feature_size=48
+data_dir=/ai/workspace/datasets/VoCo_Downstream/CHAOS/
+cache_dir=/ai/workspace/datasets/cache/CHAOS
+use_ssl_pretrained=True
+use_persistent_dataset=True
+
+mkdir -p $logdir
+
+python main.py \
+    --name $name \
+    --batch_size 4 \
+    --pretrained_root $pretrained_root \
+    --feature_size $feature_size \
+    --data_dir $data_dir \
+    --cache_dir $cache_dir \
+    --use_ssl_pretrained $use_ssl_pretrained \
+    --use_persistent_dataset $use_persistent_dataset \
+    --logdir $logdir | tee $logdir/$now.txt
